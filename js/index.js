f708ca87cabbad30c9de82f4cf6cf79a3c78c4c3
Carlos Fins <clockwerkz@gmail.com>
2020-01-18 14:57:16 -0800

    minor fixes

--- js/index.js
+++ js/index.js
@@ -1,4 +1,10 @@
 document.getElementById('login-form').addEventListener('submit', function(e) {
+
+    const usernames = ['carlos', 'tom34', 'claude'];
+    const passwords = ['123','123','123'];
+    const username = document.getElementById('username').value;
+    
+    const index = usernames.indexOf(username);
     e.preventDefault();
     
     //window.location.replace('feed.html');
