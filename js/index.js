e1850006166681ec979a5c7103aa23235c20c5b0
Carlos Fins <clockwerkz@gmail.com>
2020-01-18 12:50:04 -0800

    completes styling the login (for now)

--- js/index.js
+++ js/index.js
@@ -1,7 +1,6 @@
 document.getElementById('login-form').addEventListener('submit', function(e) {
     e.preventDefault();
-    console.log(document.getElementById('username').value);
-    localStorage.setItem('username', document.getElementById('username').value);
-    window.location.replace('feed.html');
+    
+    //window.location.replace('feed.html');
 });
 
